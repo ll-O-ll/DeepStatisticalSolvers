0615b222a52b3b1eb3c623bcd4af92fb40fa4e33
Ahmed Rosanally <arosanal@cedar1.cedar.computecanada.ca>
2022-03-07 17:03:38 -0800

    updated dependencies for Cedar cluster | modified worker script

--- tensorflow-test.sh
+++ tensorflow-test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --mail-user=ahmed.rosanally@mail.utoronto.ca # request notification by email of jobs
 #SBATCH --mail-type=ALL
-#SBATCH --gres=gpu:1        # request GPU "generic resource"
-#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M        # memory per node
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:p100:4   # request GPU "generic resource"
+#SBATCH --cpus-per-task=24   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --exclusive
+#SBATCH --mem=125G        # memory per node
 #SBATCH --time=0-03:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 
 module load cuda cudnn 
 source ENV/bin/activate
 python ./tensorflow-test.py
-
 # python ./main.py --data_dir=datasets/acpf_14 --learning_rate=3e-3 --minibatch_size=1000 --alpha=1e-2 --hidden_layers=2 --latent_dimension=10 --correction_updates=10 --track_validation=1000
